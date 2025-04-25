196b42b6de8954f45a21c73ac3c8009e324e6072
Tom Webber <t.c.w.webber@gmail.com>
2025-04-25 20:20:37 +0200

    (server) add: basic HTTP server than listens for requests

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use http_body_util::Full;
+use hyper::body::Bytes;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, String> {
+    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = TcpListener::bind(addr).await?;
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+        tokio::task::spawn(async move {
+            let builder = http1::Builder::new()
+                .serve_connection(io, service_fn(hello))
+                .await;
+            if let Err(err) = builder {
+                eprintln!(r"Error serving connection: {err:?}");
+            }
+        });
+    }
+}
